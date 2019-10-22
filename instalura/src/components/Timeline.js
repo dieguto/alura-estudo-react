04197078db5fe2fecb38e3f9a759e41b93370b33
DIEGO AUGUSTO FARIAS SILVA <18175158@CFP127ALUNO.LOCAL>
2019-10-22 16:20:28 -0300

    Login funcionando e retornando o token

--- instalura/src/components/Timeline.js
+++ instalura/src/components/Timeline.js
@@ -10,7 +10,7 @@ export default class Timeline extends Component {
 
 
     componentDidMount(){
-        fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+        fetch('https://instalura-api.herokuapp.com/api/public/fotos/alots')
         .then(response => response.json())
         .then(fotos => {
             this.setState({fotos:fotos})
--- instalura/src/index.js
+++ instalura/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/reset.css'
-import './css/timeline.css'
-
+import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css';
+import {Router, Route, browserHistory} from 'react-router';
+import Login from './components/Login';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 
-serviceWorker.unregister();
+ReactDOM.render(
+    (
+        /* historíco de navegação para podermos realizar a navegação entra as rotas */
+        <Router history={browserHistory}>
+            <Route path="/" component={Login}/>
+            <Route path="/timeline" component={App}/>
+        </Router>
+    ), 
+document.getElementById('root')
+);
+
+
+
